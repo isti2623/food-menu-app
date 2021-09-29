d2fb9405d93b72647ec7403fcc98cad57603e337
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-09-30 01:56:13 +0600

    added react router react bootstrap fontawesome  and setup all route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import Food from './components/Food/Food';
+import About from './components/About/About';
+import FoodDetails from './components/FoodDetails/FoodDetails';
+import Contact from './components/Contact/Contact';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/home">
+          <Home />
+        </Route>
+        <Route exact path="/food">
+          <Food />
+        </Route>
+        <Route exact path="/fooddetails/:id">
+          <FoodDetails />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Food/Food.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Food = () => {
+    return (
+        <div>
+            <h2>this is food</h2>
+        </div>
+    );
+};
+
+export default Food;
